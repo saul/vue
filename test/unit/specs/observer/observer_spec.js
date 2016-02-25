63cbf3196c064971e3314cf5b62419c586052ad1
Saul Rennison <saul.rennison@gmail.com>
2016-02-25 11:09:37 +0000

    Revert "Test whether a property is configurable before defining it as reactive"
    This reverts commit b359ff6cb749f0cdd3007f46fb2d69ed4d3e6e32.

--- test/unit/specs/observer/observer_spec.js
+++ test/unit/specs/observer/observer_spec.js
@@ -173,6 +173,9 @@ describe('Observer', function () {
   })
 
   it('create on property which is marked not configurable', function () {
+    var previousConvertAllProperties = config.convertAllProperties
+    config.convertAllProperties = true
+
     // on object
     var obj = {}
     Object.defineProperty(obj, 'a', {
@@ -185,6 +188,8 @@ describe('Observer', function () {
     expect(ob instanceof Observer).toBe(true)
     expect(ob.value).toBe(obj)
     expect(obj.__ob__).toBe(ob)
+
+    config.convertAllProperties = previousConvertAllProperties
   })
 
   it('create on array', function () {
